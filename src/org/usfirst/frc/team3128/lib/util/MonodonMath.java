988a9a4701c1a6b94b8c3fde04674e7ce4878241
Garrison Price <garrison.b.price@gmail.com>
2016-03-11 14:15:47 -0800

    Created Eclipse Project and Started Math util

---
+++ src/org/usfirst/frc/team3128/lib/util/MonodonMath.java
@@ -0,0 +1,147 @@
+package org.usfirst.frc.team3128.lib.util;
+
+public class MonodonMath {
+	public static float DegToRad = 0.0174533f; 
+	public static float RadToDeg = 57.2958f; 
+	public static float PI = 3.14159f;
+	
+	//TODO Returns the absolute value of f.
+	public static float Abs(float f) {
+		return f;
+	}
+	
+	//TODO Returns the arc-cosine of f - the angle in radians whose cosine is f.
+	public static float Acos(float f) {
+		return f;
+	}
+	
+	//TODO Compares two floating point values if they are similar.
+	public static boolean Approximately(float a, float b) {
+		return true;
+	}
+	
+	//TODO Returns the arc-sine of f - the angle in radians whose sine is f.
+	public static float Asin(float f) {
+		return f;
+	}
+	
+	//TODO Returns the arc-tangent of f - the angle in radians whose tangent is f.
+	public static float Atan(float f) {
+		return f;
+	}
+	
+	//TODO Returns the angle in radians whose Tan is y/x.
+	public static float Atan2(float x, float y) {
+		return x;
+	}
+	
+	//TODO 	Returns the smallest integer greater to or equal to f.
+	public static int Ceil(float f) {
+		return (int)f;
+	}
+	
+	//TODO Clamps a value between a minimum float and maximum float value.
+	public static float Clamp(float f, float min, float max) {
+		return f;
+	}
+	
+	//TODO Clamps value between 0 and 1 and returns value.
+	public static float Clamp01(float f) {
+		return f;
+	}
+	
+	//TODO Returns the closest power of two value.
+	public static float ClosestPowerOf2(float f) {
+		return f;
+	}
+	
+	//TODO Returns the cosine of angle f in radians.
+	public static float Cos(float f) {
+		return f;
+	}
+	
+	//TODO Calculates the shortest difference between two given angles given in degrees.
+	public static float DeltaAngle(float a, float b) {
+		return a;
+	}
+	
+	//TODO Returns the largest integer smaller to or equal to f.
+	public static int Floor(float f) {
+		return (int)f;
+	}
+	
+	//TODO Calculates the linear parameter t that produces the interpolant value within the range [a, b].
+	public static float InverseLerp(float f) {
+		return f;
+	}
+	
+	//TODO Returns true if the value is power of two.
+	public static boolean IsPowerOf2(int i) {
+		return true;
+	}
+	
+	//TODO Linearly interpolates between a and b by t.
+	public static float Lerp(float f) {
+		return f;
+	}
+	
+	//TODO Same as Lerp but makes sure the values interpolate correctly when they wrap around 360 degrees.
+	public static float LerpAngle(float f) {
+		return f;
+	}
+	
+	//TODO Returns the logarithm of a specified number in a specified base.
+	public static float Log(float f) {
+		return f;
+	}
+	
+	//TODO Returns the base 10 logarithm of a specified number.
+	public static float Log10(float f) {
+		return f;
+	}
+	
+	//TODO Returns largest of two or more values.
+	public static float Max(float f) {
+		return f;
+	}
+	
+	//TODO Returns the smallest of two or more values.
+	public static float Min(float f) {
+		return f;
+	}
+	
+	//TODO Returns the next power of two value.
+	public static int NextPowerOfTwo(float f) {
+		return (int)f;
+	}
+	
+	//TODO Returns f raised to power p.
+	public static float Pow(float f, float p) {
+		return f;
+	}
+	
+	//TODO Returns f rounded to the nearest integer.
+	public static int Round(float f) {
+		return (int)f;
+	}
+	
+	//TODO Returns the sign of f.
+	public static float Sign(float f) {
+		return f;
+	}
+	
+	//TODO Returns f rounded to the nearest integer.
+	public static float Sin(float f) {
+		return f;
+	}
+	
+	//TODO Returns square root of f.
+	public static float Sqrt(float f) {
+		return f;
+	}
+	
+	//TODO 	Returns the tangent of angle f in radians.
+	public static float Tan(float f) {
+		return f;
+	}
+}
